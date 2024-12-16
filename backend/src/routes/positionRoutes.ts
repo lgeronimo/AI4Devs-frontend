0e5c260e813e77dedcf772f8e9705f8ff71d215c
lgeronimo <luis.geronimov@gmail.com>
2024-12-15 23:51:01 -0600

    feat: add positions service

--- backend/src/routes/positionRoutes.ts
+++ backend/src/routes/positionRoutes.ts
@@ -1,9 +1,20 @@
 import { getCandidatesByPosition, getInterviewFlowByPosition } from '../presentation/controllers/positionController';
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
 
-
-const router = require('express').Router();
+const router = Router();
+const prisma = new PrismaClient();
 
 router.get('/:id/candidates', getCandidatesByPosition);
 router.get('/:id/interviewflow', getInterviewFlowByPosition);
+
+router.get('/', async (req, res) => {
+    try {
+        const positions = await prisma.position.findMany();
+        res.json(positions);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener las posiciones' });
+    }
+});
 
 export default router;
